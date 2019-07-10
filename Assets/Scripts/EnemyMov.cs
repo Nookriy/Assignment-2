b53f9183b4b8ba6e51245fd295bfb299dc468dfb
Nookriy <a.azman@student.saedubai.com>
2019-07-10 17:18:17 +0400

    Start

---
+++ Assets/Scripts/EnemyMov.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMov : MonoBehaviour
+{
+    public Transform[] Waypoints;
+    public Transform desiredWaypoint;
+    private NavMeshAgent ai;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ai = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AImovement();
+    }
+
+    public void AImovement()
+    {
+        if (desiredWaypoint == null)
+        {
+            desiredWaypoint = Waypoints[Random.Range(0, Waypoints.Length - 1)];
+        }
+        if (desiredWaypoint.position.x == transform.position.x && desiredWaypoint.position.z == transform.position.z)
+        {
+            desiredWaypoint = null;
+        }
+        if(desiredWaypoint != null)
+        {
+            ai.SetDestination(desiredWaypoint.position);
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerMov.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMov : MonoBehaviour
+{
+    private Rigidbody rb;
+    public GameObject[] gameObjects;
+    public float force;
+    private NavMeshAgent agent;
+    private Camera camera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        agent = gameObject.GetComponent<NavMeshAgent>();
+        camera = GameObject.FindObjectOfType<Camera>();
+
+        //Debug.Log(intfunction(gameObjects[0], gameObjects[3]));
+        //Debug.Log(intfunction(gameObjects[1], gameObjects[2]));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MouseMovement();
+    }
+
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(transform.forward * force);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(transform.forward * -force);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(transform.right * force);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(transform.right * -force);
+        }
+    }
+
+    void MouseMovement()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray,out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+    //private void disfunct()
+    //{
+       
+    //}
+
+    //private float intfunction(GameObject x, GameObject y)
+    //{
+    //    float distance;
+    //    distance = Vector3.Distance(x.transform.position, y.transform.position);
+    //    return distance;
+    //}
+}
